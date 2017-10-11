7e3af0ce3002bccd559f3dd5b51597459381c55a
longxianghui <240226543@qq.com>
2017-10-11 14:26:45 +0800

    dotnetcode

---
+++ demo3/dotnet/Orders/Order1/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Order1.Controllers
+{
+    [Route("/")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "order1";
+        }
+    }
+}
---
+++ demo3/dotnet/Orders/order2/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Order2.Controllers
+{
+    [Route("/")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "order2";
+        }
+    }
+}
