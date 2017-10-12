13aea203723381414c8229c8af9e5f252f155ee7
longxianghui <240226543@qq.com>
2017-10-12 18:07:39 +0800

    config

---
+++ demo4/dotnet/ConfigClient/ConfigClient1/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Steeltoe.Extensions.Configuration.ConfigServer;
+
+namespace ConfigClient1.Controllers
+{
+    [Route("/")]
+    public class ValuesController : Controller
+    {
+        private readonly IConfigurationRoot _config;
+        private readonly IOptionsSnapshot<Demo> _configDemo;
+
+        public ValuesController(IConfigurationRoot config, IOptionsSnapshot<Demo> configDemo)
+        {
+            _config = config;
+            _configDemo = configDemo;
+        }
+        // GET api/values
+        [HttpGet]
+        public Demo Get()
+        {
+            //两种方式获取配置文件的数据
+            //var demo = new Demo
+            //{
+            //    Name = _config["name"],
+            //    Age = int.Parse(_config["age"]),
+            //    Env = _config["env"]
+            //};
+            var demo = _configDemo.Value;
+            return demo;
+        }
+
+    
+    }
+}
